013d4adf0e3fe29621d7eebaf1089f1133415215
GS-Kanishkar <gskanishkar10@gmail.com>
2023-12-15 01:08:25 +0530

    added code for length 0

--- src/Footer.js
+++ src/Footer.js
@@ -1,16 +1,17 @@
 import React from 'react'
 
 const Footer = ({item}) => {
+
+  const itemCount = item ? item.length : 0;
   return (
     <div className='footerClass'>
-      {
-        (item && item.length<=1) ? (
-          <p>number of item : {item.length} </p>
-        ) : 
-        ( <p>number of items : { item.length}</p> )
-      }
+      {itemCount <= 1 ? (
+        <p>number of item: {itemCount}</p>
+      ) : (
+        <p>number of items: {itemCount}</p>
+      )}
     </div>
   ) 
 }
 
-export default Footer
+export default Footer 
